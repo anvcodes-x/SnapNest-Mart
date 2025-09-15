2b9c7e9b005cf36fec3fe23c9343bd8bc034bd0d
anvcodes-x <rarhoreanvesha@gmail.com>
2025-09-15 20:45:14 +0530

    Intial commit: SnapNest Mart fullstack projrct

---
+++ Backend/src/main/java/com/Anvesha/SnapNest/SnapNestApplication.java
@@ -0,0 +1,13 @@
+package com.Anvesha.SnapNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapNestApplication.class, args);
+	}
+
+}
---
+++ Backend/src/main/resources/data1.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO product (name, "DESC", brand, price, category, release_date, available, quantity)
+VALUES ('Tata Nexon', 'A compact SUV with excellent safety features', 'Tata', 1200000.00, 'Automobile', '2024-05-10', TRUE, 20);
+
+INSERT INTO product (name, "DESC", brand, price, category, release_date, available, quantity)
+VALUES ('Maruti Suzuki Swift', 'A popular hatchback known for mileage', 'Maruti', 800000.00, 'Automobile', '2024-04-15', TRUE, 35);
+
+INSERT INTO product (name, "DESC", brand, price, category, release_date, available, quantity)
+VALUES ('Samsung Galaxy S24', 'High performance flagship smartphone', 'Samsung', 79999.00, 'Electronics', '2024-03-10', TRUE, 50);
+
+INSERT INTO product (name, "DESC", brand, price, category, release_date, available, quantity)
+VALUES ('Nike Air Zoom', 'Comfortable and stylish running shoes', 'Nike', 7999.00, 'Fashion', '2024-03-20', TRUE, 120);
+
+INSERT INTO product (name, "DESC", brand, price, category, release_date, available, quantity)
+VALUES ('Sony WH-1000XM5', 'Noise cancelling wireless headphones', 'Sony', 29999.00, 'Electronics', '2023-12-05', FALSE, 0);
+
